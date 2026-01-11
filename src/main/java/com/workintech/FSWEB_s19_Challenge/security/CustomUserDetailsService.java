41dc747a15ea2db95b0820854f83761c71b6c557
sirmaatak <sirma.atak@outlook.com>
2026-01-11 12:53:51 +0300

    Security config is done

---
+++ src/main/java/com/workintech/FSWEB_s19_Challenge/security/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.workintech.FSWEB_s19_Challenge.security;
+
+import com.workintech.FSWEB_s19_Challenge.entity.User;
+import com.workintech.FSWEB_s19_Challenge.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUserName(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getFullName(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
+
